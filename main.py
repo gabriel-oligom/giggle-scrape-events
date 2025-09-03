e84b69ac25fb2be20bf74402a7f07363a7cfc7a9
Gabriel Oliveira <155106088+gabriel-oligom@users.noreply.github.com>
2025-09-02 21:54:47 -0300

    Migrate storage from text file to SQLite database

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ import smtplib, ssl
 import os
 from dotenv import load_dotenv
 import time
+import sqlite3
+
+# "INSERT INTO events VALUES ('Tigers', 'Tiger City', '2088.10.14')"
 
 load_dotenv()
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
+connection = sqlite3.connect("events.db")
 
 
 def scrape(url):
@@ -41,13 +47,20 @@ def send_email(message):
 
 
 def store(extracted):
-    with open("data.txt", "a") as file:
-        file.write(extracted + "\n")
-
+    row = extracted.split(',')
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
 
 def read(extracted):
-    with open("data.txt", "r") as file:
-        return file.read()
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events  WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    return rows
 
 
 if __name__ == "__main__":
@@ -56,9 +69,9 @@ if __name__ == "__main__":
         extracted = extract(scraped)
         print(extracted)
 
-        content = read(extracted)
         if extracted != "No upcoming tours":
-            if not extracted in content:
+            row = read(extracted)
+            if not row:
                 store(extracted)
                 send_email(message="Hey new tour was found!")
         time.sleep(2)
